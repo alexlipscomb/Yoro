82f91b7ef31474e9386ffc23adf829bef9660d15
Alex Lipscomb <79888233+alexlipscomb@users.noreply.github.com>
2023-03-09 15:12:05 -0600

    feat: Remove globalpatchername from init_slot.js

--- code/init_slot.js
+++ code/init_slot.js
@@ -2,10 +2,5 @@ var PATCHER = this.patcher;
 
 
 function init(channel, slot) {
-	PATCHER.setattr("globalpatchername", "slot_" + slot);
-	_updateTitle(channel, slot);
-}
-
-function _updateTitle(channel, slot) {
 	PATCHER.setattr("title", "(Channel " + channel + ") " + "Slot " + slot);
 }
